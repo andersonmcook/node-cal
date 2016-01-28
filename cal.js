9a72e08f28cf3bb276471e3885d783f8e13d0a83
andersonmcook <andersonmcook@gmail.com>
2016-01-28 09:12:15 -0600

    added some fail conditions

--- cal.js
+++ cal.js
@@ -6,13 +6,12 @@ const [ , , ...args] = process.argv;
 
 if (args.length === 2) {
   const[month, year] = args;
-  //console.log(month, year);
+  month > 12 || month < 1 ? (console.log("nope"), process.exit(64)) : month;
+  year < 1753 || year > 9999 ? (console.log("nope"), process.exit(64)) : year;
   console.log(`${wholeMonth(parseInt(year), parseInt(month))}`);
-  //return `${wholeMonth(parseInt(year), parseInt(month))}}`;
 } else if (args.length === 1) {
   const[year] = args;
   console.log("oh no");
-  //console.log(`generateYear(${year})`);
 } else {
   console.log('nope');
   process.exit(64);
